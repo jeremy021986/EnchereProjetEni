1b7e323f3db300258fd9226bc8d9c3b38760f4f3
vretureau2021 <vretureau2021@102SE42-JNH3P02.ad.campus-eni.fr>
2021-12-08 11:49:59 +0100

    @WebServlet(description = "Deconnexion", urlPatterns = { "/Deconnexion" }) public class UtilisateurLogoutServlet extends HttpServlet {

--- EnchereProjetEni/src/fr/eni/enchere/servlet/UtilisateurLogoutServlet.java
+++ EnchereProjetEni/src/fr/eni/enchere/servlet/UtilisateurLogoutServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class UtilisateurLogoutServlet
  */
-@WebServlet(description = "deconnexion", urlPatterns = { "/deconnexion" })
+@WebServlet(description = "Deconnexion", urlPatterns = { "/Deconnexion" })
 public class UtilisateurLogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
