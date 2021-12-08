6fc87a606d4774a88b585dcfcdd6d106540c5d49
jeremy021986 <91890860+jeremy021986@users.noreply.github.com>
2021-12-08 16:49:55 +0100

    ArticleVenduDAO

--- EnchereProjetEni/src/fr/eni/enchere/dal/ArticleVenduDAO.java
+++ EnchereProjetEni/src/fr/eni/enchere/dal/ArticleVenduDAO.java
@@ -11,8 +11,8 @@ import fr.eni.enchere.bo.ArticleVendu;
 public interface ArticleVenduDAO {
 	
 	ArticleVendu selectById(int id) throws DALException;
-	void update(ArticleVendu data) throws DALException;
-	void insert(ArticleVendu data) throws DALException;
+	void update(ArticleVendu articleVendu) throws DALException;
+	void insert(ArticleVendu articleVendu) throws DALException;
 	void delete(int id) throws DALException;
 	List<ArticleVendu> selectAll() throws DALException;
 
