51a4c7ad4bf1d00300f412dafe84af8a76bc840a
jeremy021986 <91890860+jeremy021986@users.noreply.github.com>
2021-12-09 10:09:01 +0100

    Add files via upload

---
+++ EnchereProjetEni/src/fr/eni/enchere/bll/UtilisateurDAO.java
@@ -0,0 +1,13 @@
+package fr.eni.enchere.dal;
+
+import fr.eni.enchere.bo.Utilisateur;
+
+public interface UtilisateurDAO {
+	
+	Utilisateur selectById(int idUtilisateur) throws DALException;
+	void update(Utilisateur MajUtilisateur) throws DALException;
+	void insert(Utilisateur ajoutUtilisateur) throws DALException;
+	void delete(int idUtilisateur) throws DALException;
+	Utilisateur selectBypseudo(String pseudo) throws DALException;
+	
+}
