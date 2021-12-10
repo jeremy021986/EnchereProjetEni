d696fa032e2d5b4f4f3ea8d275800cf72e558b6f
yannbernard1 <91891016+yannbernard1@users.noreply.github.com>
2021-12-10 11:18:27 +0100

    profil utilisateur affichage

--- EnchereProjetEni/WebContent/WEB-INF/jsp/ProfilUtilisateur.jsp
+++ EnchereProjetEni/WebContent/WEB-INF/jsp/ProfilUtilisateur.jsp
@@ -1,6 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+    <%@ page import="java.util.List" %>
+<%@ page import="fr.eni.enchere.bo.Utilisateur" %>    
+<%@ page import="java.time.format.FormatStyle" %> 
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,16 +15,18 @@
     <title>Document</title>
 </head>
 <body>
-    <h1> ENI-Encheres</h1>
-    <h4>Pseudo :</h4>
-    <h4>Nom :</h4>
-    <h4>Prénom :</h4>
-    <h4>Email :</h4>
-    <h4>Téléphone :</h4>
-    <h4>Rue :</h4>
-    <h4>Code Postal :</h4>
-    <h4>Ville :</h4>
-   
+<h1> ENI-Encheres</h1>
+
+    <h4>Pseudo :</h4><c:out value="${sessionScope.pseudo}"/><br>
+  	<h4> Nom :</h4><c:out value="${requestScope.nom}"/><br>
+    <h4>Prénom :</h4><c:out value="${requestScope.prenom}"/><br>
+    <h4>Email :</h4><c:out value="${requestScope.email}"/><br>
+    <h4>Téléphone :</h4><c:out value="${requestScope.tel}"/><br>
+    <h4>Rue :</h4><c:out value="${requestScope.rue}"/><br>
+    <h4>Code Postal :</h4><c:out value="${requestScope.cp}"/><br>
+    <h4>Ville :</h4><c:out value="${requestScope.ville}"/><br>
+    
+ 
     <a href="./ModifProfilUtilisateur"><button type="button"
 					class="btn btn-primary btn-block">Modifier mon profil</button></a> <br />
 
--- EnchereProjetEni/src/fr/eni/enchere/servlet/ProfilServlet.java
+++ EnchereProjetEni/src/fr/eni/enchere/servlet/ProfilServlet.java
@@ -1,6 +1,7 @@
 package fr.eni.enchere.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.enchere.bll.BLLException;
+import fr.eni.enchere.bll.UtilisateurManager;
+import fr.eni.enchere.bo.Utilisateur;
 
 /**
  * Servlet implementation class ProfilServlet
@@ -17,21 +23,55 @@ public class ProfilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String pseudo = (String) session.getAttribute("pseudo");
+			try {
+				Utilisateur utilisateur = UtilisateurManager.getInstance().afficherProfil(pseudo);
+				request.setAttribute("pseudo", pseudo);
+				String nom = utilisateur.getNom();
+				request.setAttribute("nom", nom);
+				String prenom = utilisateur.getPrenom();
+				request.setAttribute("prenom", prenom);
+				String email = utilisateur.getEmail();
+				request.setAttribute("email", email);
+				String tel = utilisateur.getTelephone();
+				request.setAttribute("tel", tel);
+				String rue = utilisateur.getRue();
+				request.setAttribute("rue", rue);
+				String cp = utilisateur.getCodePostal();
+				request.setAttribute("cp", cp);
+				String ville = utilisateur.getVille();
+				request.setAttribute("ville", ville);
+			
+				
+				
+				
+				
+			} catch (BLLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/jsp/ProfilUtilisateur.jsp");
 		dispatcher.forward(request, response);
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+
 	}
 
 }
