bce8aaec6fa6d8df14f8ff8ade8c902767ec2b47
yannbernard1 <91891016+yannbernard1@users.noreply.github.com>
2021-12-17 11:16:18 +0100

    delete servlet

--- EnchereProjetEni/src/fr/eni/enchere/servlet/SupProfilServlet.java
+++ EnchereProjetEni/src/fr/eni/enchere/servlet/SupProfilServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.enchere.bll.BLLException;
+import fr.eni.enchere.bll.UtilisateurManager;
+import fr.eni.enchere.bo.Utilisateur;
 
 /**
  * Servlet implementation class SupProfilServlet
@@ -17,20 +22,31 @@ public class SupProfilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String pseudo = (String) session.getAttribute("pseudo");
+		try {
+			Utilisateur user = UtilisateurManager.getInstance().affichageProfil(pseudo);
+			UtilisateurManager.getInstance().delete(user.getIdUtilisateur());
+		} catch (BLLException e) {
+			e.printStackTrace();
+		}
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/jsp/PageAccueil.jsp");
 		dispatcher.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
